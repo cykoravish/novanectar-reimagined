183a448f2fb70eebd9eb08ae194ac1d76ce6c6ac
Ravish Bisht <ravishbisht85@gmail.com>
2025-01-11 10:11:41 +0530

    about session made

--- src/components/landing/MainContent.tsx
+++ src/components/landing/MainContent.tsx
@@ -1,8 +1,11 @@
 "use client";
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
 // import { motion } from "framer-motion";
+import techLife from "@/assets/tech-life.png";
+import dotts from "@/assets/dotted-background.png";
 
 import { gsap } from "gsap";
+import Image from "next/image";
 import { useEffect } from "react";
 
 export const MainContent = () => {
@@ -55,30 +58,66 @@ export const MainContent = () => {
     //     </div>
     //   </div>
     // </div>
-    <div>
-      <div className="h-[35rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <h1 className="pt-28 text-black text-4xl text-center font-semibold">
-          We Build success Stories Not Just Websites
-        </h1>
-        <p className="text-black text-center font-medium text-lg pt-10">
-          Transform your business with powerfull digital strategies designed for
-          real world result
-        </p>
-        <div className="flex pt-10 justify-center gap-4">
-          <button className="bg-blue-500 px-4 py-3 text-white rounded-md">
-            Get Started Toaday
-          </button>
-          <button className="border border-blue-500 text-blue-500 px-4 py-3 rounded-md font-medium">
-            Schedule a free Consultant
-          </button>
+    <>
+      <div>
+        <div className="h-[90rem] w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <h1 className="pt-28 text-black text-4xl text-center font-semibold">
+            We Build success Stories Not Just Websites
+          </h1>
+          <p className="text-black text-center font-medium text-lg pt-10">
+            Transform your business with powerfull digital strategies designed
+            for real world result
+          </p>
+          <div className="flex pt-10 justify-center gap-4">
+            <button className="bg-blue-500 px-4 py-3 text-white rounded-md">
+              Get Started Toaday
+            </button>
+            <button className="border border-blue-500 text-blue-500 px-4 py-3 rounded-md font-medium">
+              Schedule a free Consultant
+            </button>
+          </div>
+          <p className="text-black text-center pt-8">
+            Building Success Stories for
+            <span className="text-blue-500">200+ Trusted clients</span>
+          </p>
+          <div className="px-4 mt-10 pb-28">
+            <h1 className="text-black text-4xl font-semibold underline">
+              About
+            </h1>
+            <div className="flex">
+              <div>
+                <p className="text-black pt-10 text-2xl font-medium">
+                  We provide smart digital solutions to help your business grow.
+                </p>
+                <p className="text-black text-lg pt-4">
+                  We specialize in a wide range of design development, including
+                  UI/UX, Graphic Design, 2D/3D Illustration & Animation, Logo &
+                  Brand Identity Design, Web Development, App Development, SEO
+                  and more.
+                </p>
+              </div>
+              <Image
+                src={techLife}
+                alt="techLife"
+                width={150}
+                height={200}
+                className="md:block hidden"
+              />
+            </div>
+          </div>
+            <Image
+              src={dotts}
+              alt="dotted"
+              width={150}
+              height={200}
+              className="md:block hidden"
+            />
+          <div className="bg-blue-300 h-[30rem] pt-16 text-black text-3xl text-center">
+            <h1 className="font-semibold underline">Services</h1>
+          </div>
         </div>
-        <p className="text-black text-center pt-8">
-          Building Success Stories for{" "}
-          <span className="text-blue-500">200+ Trusted clients</span>
-        </p>
       </div>
-      <h1>About</h1>
-    </div>
+    </>
   );
 };
